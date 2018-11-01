778ddc64e79631fa0a4699eb5d8e518b7c5d8e9c
Leslie Osorio <lesosorio17@gmail.com>
2018-11-01 21:13:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,10 +9,10 @@ end
 collection
 end
 
-my_collect(["ruby","javascript","python","objective-c"]) do |lang|
-  lang.upcase
-end
-
-my_collect(["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]) do |name|
-  name.split(" ").first
-end
+# my_collect(["ruby","javascript","python","objective-c"]) do |lang|
+#   lang.upcase
+# end
+#
+# my_collect(["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]) do |name|
+#   name.split(" ").first
+# end
